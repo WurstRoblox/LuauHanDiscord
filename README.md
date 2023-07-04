26c39a6eec52d1528b011654aba93a3b0ae8c87b
heckeroncrack <124959846+heckeroncrack@users.noreply.github.com>
2023-07-05 01:15:22 +0200

    wa

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # LuauHanDiscord
 LuauHanDiscord is a Luau Handicapped Discord wrapper
+## No Secret Documentation
+### Setup
+To setup the wrapper start your code with:     
+```lua
+local wrapper = loadstring(game:HttpGet("https://github.com/WurstRoblox/LuauHanDiscord/raw/main/wrapper.lua"))()
+```
+### Send message
+To send a message do this:     
+```lua
+wrapper:send("channel id", "message", "bot token")
+```
+## With Secret Documentation
+### Setup
+To setup the wrapper start your code with:     
+```lua
+local wrapper = loadstring(game:HttpGet("https://github.com/WurstRoblox/LuauHanDiscord/raw/main/wrapper.lua"))()
+wrapper:Connect("token", "your secret")
+```      
+In this part, you can make your secret key whatever, you will use this later on.     
+Make sure your secret is not distributed, as anyone can find your token using your secret.
+### Send message
+To send a message do this:     
+```lua
+wrapper:s_send("channel id", "message", "your secret")
+```
--- wrapper.lua
+++ wrapper.lua
@@ -1,1 +1,35 @@
-
+local wrapper = {
+    request = syn and syn.request or http and http.request or http_request or fluxus and fluxus.request or getgenv().request or request
+}
+function wrapper:send(wa,na,token)
+	local Response = requestfunc({
+		Url = "https://discord.com/api/v10/channels/"..wa.."/messages",
+		Method = "POST",
+		Headers = {
+			Authorization = "Bot "..token,
+			["Content-Type"] = "application/json"
+		},
+		Body = game.HttpService:JSONEncode({
+			content = na
+		})
+	})
+	return Response.Body
+end
+function wrapper:s_send(wa,na,token)
+	local Response = requestfunc({
+		Url = "https://discord.com/api/v10/channels/"..wa.."/messages",
+		Method = "POST",
+		Headers = {
+			Authorization = "Bot "..shared["secr"..token.."et"],
+			["Content-Type"] = "application/json"
+		},
+		Body = game.HttpService:JSONEncode({
+			content = na
+		})
+	})
+	return Response.Body
+end
+function wrapper:Connect(token,secretkey)
+    shared["secr"..secretkey.."et"] = token
+end
+return wrapper
